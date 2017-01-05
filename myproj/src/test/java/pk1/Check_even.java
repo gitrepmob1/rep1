34294248de1803809d5bee4a3e5837079007c365
venkatgn <venkatadrin.g@gmail.com>
2017-01-05 19:21:20 +0530

    user1 1st time upload

---
+++ myproj/src/test/java/pk1/Check_even.java
@@ -0,0 +1,16 @@
+package pk1;
+
+public class Check_even 
+{
+
+	public static void main(String[] args)
+	{
+	  int n=8;
+	  if(n%2==0)
+		  System.out.println("it is even");
+	  else
+		  System.out.println("it is odd");
+
+	}
+
+}
